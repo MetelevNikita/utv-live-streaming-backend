1c068325b541adf09bbf7e0f5441e648ebed835d
MetelevNikita <Kyle.B@mail.ru>
2024-06-17 12:04:32 +0500

    path to personimg

--- src/Controller/teamController.js
+++ src/Controller/teamController.js
@@ -32,7 +32,7 @@ const createTeamCard = async (req, res) => {
   const file = req.file
   console.log(req.file)
 
-  const urlFile = `https://localhost:3000/${file.filename}`
+  const urlFile = `https://www.utvls.tw1.su/${file.filename}`
 
   const newTeamCard = await Pool.query('INSERT INTO team (name, profession, image) VALUES ($1, $2, $3) RETURNING *', [name, profession, urlFile])
 
